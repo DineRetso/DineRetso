2591fd1cfb9786284c961b89f4ad40c917ae7ca0
DineRetso <dineretso04@gmail.com>
2023-11-09 18:44:57 +0800

    m

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use("/api/payment", PaymentRoutes);
 
 app.use(express.static(path.join(__dirname, "/client/build")));
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "client/build/index.html"))
+  res.sendFile(path.join(__dirname, "/client/build/index.html"))
 );
 
 app.use((err, req, res, next) => {
